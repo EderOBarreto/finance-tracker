f77697bd54c934e016414e79400f5cbf1b7b6850
eder <ederoliveirabarreto@gmail.com>
2021-11-21 11:36:20 -0300

    Add functionality to remove stock tracking

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  resources :user_stocks, only:[:create]
+  resources :user_stocks, only: [:create, :destroy]
   devise_for :users
   root to: "welcome#index"
   get "my_portfolio", to: "users#my_portfolio"
