d3f46209f5e1814cde988a6a35aa98dec6e6ce57
eder <ederoliveirabarreto@gmail.com>
2021-11-22 09:24:22 -0300

    Add friend search field and show initial error response

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,25 @@ class UsersController < ApplicationController
   def my_friends
     @friends = current_user.friends
   end
+
+  def search
+    if params[:friend].present?
+      @friend = User.find_by(first_name: params[:friend], email: params[:friend] )
+      if @friend
+        respond_to do |format|
+          format.js { render partial: 'friends/result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "User not found"
+          format.js { render partial: 'friends/result' }
+        end
+      end
+    else
+      respond_to do |format| 
+        flash.now[:alert] = "Please enter the name or email."
+        format.js { render partial: 'friends/result' }
+      end
+    end
+  end
 end
